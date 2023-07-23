515afb067871a4cb3896d69ff11612358ea27a02
Juan Pablo Peña F <pablo27-99@hotmail.com>
2023-07-23 11:44:42 -0500

    Initialize project

---
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -0,0 +1,54 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
+
+const users = [
+    {
+        userName: 'midudev',
+        name: 'Miguel Ángel Durán',
+        isFollowing: true
+    },
+    {
+        userName: 'pheralb',
+        name: 'Pablo Hernández',
+        isFollowing: false
+    },
+    {
+        name: 'Elon Musk',
+        isFollowing: false
+    }
+]
+
+export function App () {
+    const messi = { userName: 'leomessi', initialIsFollowing: true}
+
+    return (
+        <section className='App'>
+            <TwitterFollowCard userName='midudev' initialIsFollowing >
+                Miguel Ángel Durán
+            </TwitterFollowCard>
+            <TwitterFollowCard userName='pheralb' initialIsFollowing={false}>
+                Pablo Hernández
+            </TwitterFollowCard>
+            <TwitterFollowCard initialIsFollowing={false}>
+                Elon Musk
+            </TwitterFollowCard>
+            <TwitterFollowCard {...messi} >
+                Lionel Messi
+            </TwitterFollowCard>
+            {/* Using arrays */}
+            {
+                users.map(({ userName, name, isFollowing }) => {
+                    return (
+                        <TwitterFollowCard 
+                            key={userName}
+                            userName={userName}
+                            initialIsFollowing={isFollowing}
+                        >
+                            {name}
+                        </TwitterFollowCard>
+                    )
+                })
+            }
+        </section>
+    )
+}
