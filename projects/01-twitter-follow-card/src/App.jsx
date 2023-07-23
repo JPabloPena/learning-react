b764302234bc5286850443f3996e007d7eda53b7
Juan Pablo Peña F <pablo27-99@hotmail.com>
2023-07-23 12:57:53 -0500

    Lint code

--- projects/01-twitter-follow-card/src/App.jsx
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -2,53 +2,49 @@ import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
 const users = [
-    {
-        userName: 'midudev',
-        name: 'Miguel Ángel Durán',
-        isFollowing: true
-    },
-    {
-        userName: 'pheralb',
-        name: 'Pablo Hernández',
-        isFollowing: false
-    },
-    {
-        name: 'Elon Musk',
-        isFollowing: false
-    }
+  {
+    userName: 'midudev',
+    name: 'Miguel Ángel Durán',
+    isFollowing: true
+  },
+  {
+    userName: 'pheralb',
+    name: 'Pablo Hernández',
+    isFollowing: false
+  },
+  {
+    name: 'Elon Musk',
+    isFollowing: false
+  }
 ]
 
-export function App() {
-    const messi = { userName: 'leomessi', initialIsFollowing: true }
+export function App () {
+  const messi = { userName: 'leomessi', initialIsFollowing: true }
 
-    return (
-        <section className='App'>
-            <TwitterFollowCard userName='midudev' initialIsFollowing >
-                Miguel Ángel Durán
-            </TwitterFollowCard>
-            <TwitterFollowCard userName='pheralb' initialIsFollowing={false}>
-                Pablo Hernández
-            </TwitterFollowCard>
-            <TwitterFollowCard initialIsFollowing={false}>
-                Elon Musk
-            </TwitterFollowCard>
-            <TwitterFollowCard {...messi} >
-                Lionel Messi
-            </TwitterFollowCard>
-            {/* Using arrays */}
-            {
-                users.map(({ userName, name, isFollowing }) => {
-                    return (
-                        <TwitterFollowCard
-                            key={userName}
-                            userName={userName}
-                            initialIsFollowing={isFollowing}
-                        >
-                            {name}
-                        </TwitterFollowCard>
-                    )
-                })
-            }
-        </section>
-    )
+  return (
+    <section className='App'>
+      <TwitterFollowCard userName='midudev' initialIsFollowing>
+        Miguel Ángel Durán
+      </TwitterFollowCard>
+      <TwitterFollowCard userName='pheralb' initialIsFollowing={false}>
+        Pablo Hernández
+      </TwitterFollowCard>
+      <TwitterFollowCard initialIsFollowing={false}>
+        Elon Musk
+      </TwitterFollowCard>
+      <TwitterFollowCard {...messi}>Lionel Messi</TwitterFollowCard>
+      {/* Using arrays */}
+      {users.map(({ userName, name, isFollowing }) => {
+        return (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        )
+      })}
+    </section>
+  )
 }
--- projects/01-twitter-follow-card/src/TwitterFollowCard.jsx
+++ projects/01-twitter-follow-card/src/TwitterFollowCard.jsx
@@ -1,37 +1,42 @@
 import { useState } from 'react'
 
-export function TwitterFollowCard ({ children, userName = 'unkwown', initialIsFollowing }) {
-    const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
+export function TwitterFollowCard ({
+  children,
+  userName = 'unkwown',
+  initialIsFollowing
+}) {
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
 
-    const text = isFollowing ? 'Following' : 'Follow'
-    const buttonClassName = isFollowing
-        ? 'tw-followCard-button is-following'
-        : 'tw-followCard-button'
-    const imageScr = `https://unavatar.io/${userName}`
+  const text = isFollowing ? 'Following' : 'Follow'
+  const buttonClassName = isFollowing
+    ? 'tw-followCard-button is-following'
+    : 'tw-followCard-button'
+  const imageScr = `https://unavatar.io/${userName}`
 
-    const handleClick = () => {
-        setIsFollowing(!isFollowing)
-    }
+  const handleClick = () => {
+    setIsFollowing(!isFollowing)
+  }
 
-    return (
-        <article className='tw-followCard'>
-            <header className='tw-followCard-header'>
-                <img 
-                    className='tw-followCard-avatar'
-                    src={imageScr} 
-                    alt="Midudev's avatar" />
-                <div className='tw-followCard-info'>
-                    <strong>{children}</strong>
-                    <span className='tw-followCard-infoUserName'>@{userName}</span>
-                </div>
-            </header>
+  return (
+    <article className='tw-followCard'>
+      <header className='tw-followCard-header'>
+        <img
+          className='tw-followCard-avatar'
+          src={imageScr}
+          alt="Midudev's avatar"
+        />
+        <div className='tw-followCard-info'>
+          <strong>{children}</strong>
+          <span className='tw-followCard-infoUserName'>@{userName}</span>
+        </div>
+      </header>
 
-            <aside>
-                <button className={buttonClassName} onClick={handleClick}>
-                    <span className='tw-followCard-text'>{text}</span>
-                    <span className='tw-followCard-stopFollow'>Unfollow</span>
-                </button>
-            </aside>
-        </article>
-    )
+      <aside>
+        <button className={buttonClassName} onClick={handleClick}>
+          <span className='tw-followCard-text'>{text}</span>
+          <span className='tw-followCard-stopFollow'>Unfollow</span>
+        </button>
+      </aside>
+    </article>
+  )
 }
