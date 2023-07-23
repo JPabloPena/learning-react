c70a1ab014193ddb176bdb9ba42b14d28723bcbc
Juan Pablo Peña F <pablo27-99@hotmail.com>
2023-07-23 17:22:44 -0500

    Extract to components

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,57 +1,22 @@
 import { useState } from 'react'
-import './App.css'
-
-const TURNS = {
-  X: 'x',
-  O: 'o'
-}
-
-const WINNER_COMBOS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [1, 4, 8],
-  [2, 4, 6],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8]
-]
-
-const Square = ({ children, isSelected, updateBoard, index }) => {
-  const className = `square ${isSelected ? 'is-selected' : ''}`
-
-  const handleClick = () => {
-    updateBoard(index)
-  }
-
-  return (
-    <div onClick={handleClick} className={className}>
-      {children}
-    </div>
-  )
-}
+import confetti from 'canvas-confetti'
+import { TURNS } from './constants'
+import { Board } from './components/Board.jsx'
+import { checkWinner, checkEndGame } from './logic/board.js'
 
 function App () {
   const [board, setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X)
   const [winner, setWinner] = useState(null)
 
-  const checkWinner = (boardToCheck) => {
-    for (const combo of WINNER_COMBOS) {
-      const [a, b, c] = combo
-      if (
-        boardToCheck[a] &&
-        boardToCheck[a] === boardToCheck[b] &&
-        boardToCheck[a] === boardToCheck[c]
-      ) {
-        return boardToCheck[a]
-      }
-    }
-    return null
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
   }
 
   const updateBoard = (index) => {
-    // if position has something, doesn't update
+  // if position has something, doesn't update
     if (board[index] || winner) return
 
     // update board
@@ -66,33 +31,20 @@ function App () {
     // check winner
     const newWinner = checkWinner(newBoard)
     if (newWinner) {
+      confetti()
       setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
     }
   }
-
   return (
-    <main className='board'>
-      <h1>Tic tac toe</h1>
-      <section className='game'>
-        {
-          board.map((_, index) => {
-            return (
-              <Square
-                key={index}
-                index={index}
-                updateBoard={updateBoard}
-              >
-                {board[index]}
-              </Square>
-            )
-          })
-        }
-      </section>
-      <section className='turn'>
-        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
-        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
-      </section>
-    </main>
+    <Board
+      turn={turn}
+      board={board}
+      winner={winner}
+      updateBoard={updateBoard}
+      resetGame={resetGame}
+    />
   )
 }
 
---
+++ projects/02-tic-tac-toe/src/components/Board.jsx
@@ -0,0 +1,33 @@
+import { Square } from './Square.jsx'
+import { WinnerModal } from './WinnerModal.jsx'
+import { TURNS } from '../constants.js'
+
+export const Board = ({ turn, board, winner, updateBoard, resetGame }) => {
+  return (
+    <main className='board'>
+      <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Reset game</button>
+      <section className='game'>
+        {
+          board.map((square, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {square}
+              </Square>
+            )
+          })
+        }
+      </section>
+
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+      <WinnerModal winner={winner} resetGame={resetGame} />
+    </main>
+  )
+}
