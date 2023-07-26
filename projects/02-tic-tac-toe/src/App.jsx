62d7b8a889122acff300d789ab3a6507c8359a82
Juan Pablo Peña F <pablo27-99@hotmail.com>
2023-07-25 19:39:33 -0500

    Extract files to save to storage

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -3,16 +3,29 @@ import confetti from 'canvas-confetti'
 import { TURNS } from './constants'
 import { Board } from './components/Board.jsx'
 import { checkWinner, checkEndGame } from './logic/board.js'
+import { saveGameToStorage, resetGameStorage } from './logic/storage/index.js'
 
 function App () {
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    console.log({ boardFromStorage })
+    return boardFromStorage
+      ? JSON.parse(boardFromStorage)
+      : Array(9).fill(null)
+  })
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    console.log({ turnFromStorage })
+    return turnFromStorage ?? TURNS.X
+  })
   const [winner, setWinner] = useState(null)
 
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    resetGameStorage()
   }
 
   const updateBoard = (index) => {
@@ -27,6 +40,12 @@ function App () {
     // change turn
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    // save game
+    saveGameToStorage({
+      board: newBoard,
+      turn: newTurn
+    })
 
     // check winner
     const newWinner = checkWinner(newBoard)
