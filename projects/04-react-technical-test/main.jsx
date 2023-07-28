35fe4e77cb8551603f004a47a17bc201a45157b9
Juan Pablo Peña F <pablo27-99@hotmail.com>
2023-07-27 22:37:34 -0500

    Test done!

--- projects/04-react-technical-test/main.jsx
+++ projects/04-react-technical-test/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { App } from './src/App.jsx'
+import { App2 } from './src/App2.jsx'
 
 const root = createRoot(document.getElementById('app'))
-root.render(<App />)
+root.render(<App2 />)
--- projects/04-react-technical-test/src/App2.jsx
+++ projects/04-react-technical-test/src/App2.jsx
@@ -3,10 +3,11 @@ import './App.css'
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 
-export function App () {
+export function App2 () {
   const [fact, setFact] = useState()
   const [age, setAge] = useState()
 
+  // get fact when page loads
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then(res => {
@@ -16,17 +17,21 @@ export function App () {
       .then(data => {
         const { fact } = data
         setFact(fact)
-
-        const firstWord = fact.split(' ', 1)[0]
-
-        fetch(`https://api.agify.io/?name=${firstWord}`)
-          .then(res => res.json())
-          .then(data => {
-            const { age: resultantAge } = data
-            setAge(resultantAge)
-          })
       })
   }, [])
+
+  // get age whem we already have the fact
+  useEffect(() => {
+    if (!fact) return
+    const firstWord = fact.split(' ', 1)[0]
+
+    fetch(`https://api.agify.io/?name=${firstWord}`)
+      .then(res => res.json())
+      .then(data => {
+        const { age: resultantAge } = data
+        setAge(resultantAge)
+      })
+  }, [fact])
 
   return (
     <main>
