0cca08a0bfe5bab4cd5f22ced3454dd8a83c8baa
Juan Pablo Peña F <pablo27-99@hotmail.com>
2023-08-02 22:10:26 -0500

    Fetch movies

--- projects/05-react-movie-browser/src/App.jsx
+++ projects/05-react-movie-browser/src/App.jsx
@@ -1,17 +1,66 @@
 import './App.css'
+import { Movies } from './components/Movies.jsx'
+import { useMovies } from './hooks/useMovies.js'
+import { useEffect, useRef, useState } from 'react'
+
+function useSearch () {
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === ''
+      return
+    }
+
+    if (search === '') {
+      setError('You can\'t search an empty movie')
+      return
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError('You can\'t search a movie with a number')
+      return
+    }
+
+    if (search.length < 3) {
+      setError('Search must have 3 characters at least')
+      return
+    }
+
+    setError(null)
+  }, [search])
+
+  return { search, updateSearch, error }
+}
 
 function App () {
+  const { search, updateSearch, error } = useSearch()
+  const { movies, getMovies } = useMovies({ search })
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    getMovies()
+  }
+
+  const handleChange = (event) => {
+    updateSearch(event.target.value)
+  }
+
   return (
     <div className='page'>
       <header>
-        <h1>Movies browser</h1>
-        <form className='form' />
-        <input placeholder='Avengers, Star Wars, The Matrix...' />
-        <button type='submit'>Search</button>
+        <h1 className='title'>Movies browser</h1>
+        <form className='form' onSubmit={handleSubmit} autoComplete='off'>
+          <input onChange={handleChange} value={search} name='query' placeholder='Avengers, Star Wars, The Matrix...' />
+          <button type='submit'>Search</button>
+        </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
 
       <main>
-        Movies will appear here!
+        <Movies movies={movies} />
       </main>
     </div>
   )
---
+++ projects/05-react-movie-browser/src/hooks/useMovies.js
@@ -0,0 +1,30 @@
+import withResults from '../mocks/with-results.json'
+import withoutResults from '../mocks/no-results.json'
+import { useState } from 'react'
+
+export function useMovies ({ search }) {
+  const [responseMovies, setResponseMovies] = useState([])
+  const movies = responseMovies.Search
+
+  const mappedMovies = movies?.map(movie => ({
+    id: movie.imdbID,
+    title: movie.Title,
+    year: movie.Year,
+    poster: movie.Poster
+  }))
+
+  const getMovies = () => {
+    if (search) {
+      // setResponseMovies(withResults)
+      fetch(`https://www.omdbapi.com/?apikey=2d92b768&s=${search}`)
+        .then(res => res.json())
+        .then(json => {
+          setResponseMovies(json)
+        })
+    } else {
+      setResponseMovies(withoutResults)
+    }
+  }
+
+  return { movies: mappedMovies, getMovies }
+}
