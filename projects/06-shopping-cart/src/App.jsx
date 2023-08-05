df68306f1bb17145b05ea4a3d58b56327b9e59f9
Juan Pablo Peña F <pablo27-99@hotmail.com>
2023-08-05 00:36:55 -0500

    Display products

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,6 +1,34 @@
+import { useState } from 'react'
+import { Products } from './components/Products'
+import { Header } from './components/Header'
+import { products as initialProducts } from './mocks/products.json'
+
 function App () {
+  const [products] = useState(initialProducts)
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0
+  })
+
+  const filterProducts = (products) => {
+    return products.filter(product => {
+      return (
+        product.price >= filters.minPrice &&
+        (
+          filters.category === 'all' ||
+          product.category === filters.category
+        )
+      )
+    })
+  }
+
+  const filteredProducts = filterProducts(products)
+
   return (
-    <h1>Shopping Cart</h1>
+    <>
+      <Header changeFilters={setFilters} />
+      <Products products={filteredProducts} />
+    </>
   )
 }
 
