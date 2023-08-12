416b38978b90e6e10587cce89fd2bc30b01c816c
Juan Pablo Peña F <pablo27-99@hotmail.com>
2023-08-12 01:04:28 -0500

    Add local persistence for the cart

--- projects/06-shopping-cart/src/reducers/cart.js
+++ projects/06-shopping-cart/src/reducers/cart.js
@@ -1,10 +1,15 @@
 // The initial state can be any data type
-export const cartInitialState = []
+export const cartInitialState = JSON.parse(window.localStorage.getItem('cart')) || []
 
 export const CART_ACTION_TYPES = {
   ADD_TO_CART: 'ADD_TO_CART',
   REMOVE_FROM_CART: 'REMOVE_FROM_CART',
   CLEAR_CART: 'CLEAR_CART'
+}
+
+// update localStorage with state for cart
+export const updateLocalStorage = state => {
+  window.localStorage.setItem('cart', JSON.stringify(state))
 }
 
 export const cartReducer = (state, action) => {
@@ -18,24 +23,31 @@ export const cartReducer = (state, action) => {
         // Do not use cart = ... 'cause we can not mutate a state
         const newState = structuredClone(state)
         newState[productInCartIndex].quantity += 1
+        updateLocalStorage(newState)
         return newState
       }
 
-      return [
+      const newState = [
         ...state,
         {
           ...actionPayload,
           quantity: 1
         }
       ]
+
+      updateLocalStorage(newState)
+      return newState
     }
 
     case CART_ACTION_TYPES.REMOVE_FROM_CART: {
       const { id } = actionPayload
-      return state.filter(item => item.id !== id)
+      const newState = state.filter(item => item.id !== id)
+      updateLocalStorage(newState)
+      return newState
     }
 
     case CART_ACTION_TYPES.CLEAR_CART: {
+      updateLocalStorage(cartInitialState)
       return cartInitialState
     }
   }
